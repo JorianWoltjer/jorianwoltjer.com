8c57051b59c8a70c2d2b253fdd400770f8c21785
JorianWoltjer <26067369+JorianWoltjer@users.noreply.github.com>
2024-01-10 16:41:09 +0100

    Set response headers on nginx instead of frontend (was missing on backend before...)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,37 +4,6 @@ const nextConfig = {
   images: {
     domains: ["nginx"],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*{/}?',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' ${process.env.NODE_ENV === 'development' ? "'unsafe-eval'" : ''};
-              style-src 'self' 'unsafe-inline';
-              object-src 'none';
-              connect-src 'self' http://localhost:8000 ws://localhost:8000 wss://localhost:8000 ws://jorianwoltjer.com wss://jorianwoltjer.com;
-              font-src 'self' fonts.gstatic.com;
-              img-src 'self' data:;
-              frame-src www.youtube-nocookie.com;
-              frame-ancestors 'none';
-          `.replace(/\s+/g, ' ').trim(),
-          },
-        ],
-      }
-    ]
-  },
   async redirects() {
     return [
       {
