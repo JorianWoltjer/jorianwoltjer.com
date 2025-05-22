fa18c46c5b928bd41f9453154e872cf4cef12f8a
JorianWoltjer <26067369+JorianWoltjer@users.noreply.github.com>
2025-05-22 20:12:59 +0200

    Fix revalidate script and add <video> style

--- scripts/revalidate.sh
+++ scripts/revalidate.sh
@@ -8,6 +8,6 @@ fi
 
 . $(dirname $BASH_SOURCE)/../.env
 
-docker exec jw-backend curl -s -X POST http://frontend/api/revalidate -H 'Content-Type: application/json' -d '[{"type":"Custom","slug":"'"$1"'"}]' -H "X-Internal: $INTERNAL_TOKEN"
+docker exec jw-backend curl -s -X POST http://frontend/api/revalidate -H 'Content-Type: application/json' -d '{"slugs": [{"type":"Custom","slug":"'"$1"'"}]}' -H "X-Internal: $INTERNAL_TOKEN"
 
 echo "Revalidated $1"
